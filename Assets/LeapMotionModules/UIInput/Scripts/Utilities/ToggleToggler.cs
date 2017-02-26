de276ff3832e365534c111d18f1de163a7c5aec8
EthanGA <ethan.gossh@gmail.com>
2017-02-26 15:37:01 -0500

    commit

---
+++ Assets/LeapMotionModules/UIInput/Scripts/Utilities/ToggleToggler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Leap.Unity.InputModule {
+  public class ToggleToggler : MonoBehaviour {
+    public Text text;
+    public UnityEngine.UI.Image image;
+    public Color OnColor;
+    public Color OffColor;
+
+    public void SetToggle(Toggle toggle) {
+      if (toggle.isOn) {
+        text.text = "Brake On";
+        text.color = Color.white;
+        image.color = OnColor;
+      } else {
+        text.text = "Brake Off";
+        text.color = Color.white;
+        image.color = OffColor;
+      }
+    }
+  }
+}
---
+++ Assets/SphereController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SphereController : MonoBehaviour {
+
+	public Rigidbody Sphere;
+	
+	public Slider Slider;
+	public Button Top;
+	public Button Bottom;
+	public Button Left;
+	public Button Right;
+	public Toggle Stop;
+
+	
+	private float speed = 1f;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		Slider.onValueChanged.AddListener(delegate {UpdateSphereSpeed ();});
+		Top.onClick.AddListener(TopClick);
+		Bottom.onClick.AddListener(BottomClick);
+		Left.onClick.AddListener(LeftClick);
+		Right.onClick.AddListener(RightClick);
+		Stop.onValueChanged.AddListener(delegate {StopSphere ();});
+	}
+
+	public void UpdateSphereSpeed() {
+		speed = GetComponent<Slider>().value;
+
+	}
+
+	public void TopClick() {
+		Sphere.AddForce(transform.forward * speed/10, ForceMode.Impulse);
+	}
+
+	public void BottomClick() {
+		Sphere.AddForce(transform.forward * -(speed)/10, ForceMode.Impulse);
+	}
+
+	public void LeftClick() {
+		Sphere.AddForce(transform.right * -(speed)/10, ForceMode.Impulse);
+	}
+
+	public void RightClick() {
+		Sphere.AddForce(transform.right * speed/10, ForceMode.Impulse);
+	}
+
+	public void StopSphere() {
+		Vector3 velocity = Sphere.velocity; 
+		Sphere.AddForce(-velocity, ForceMode.Impulse);
+		if (Stop.isOn) {
+			Sphere.constraints = RigidbodyConstraints.FreezeAll;
+		} else {
+			Sphere.constraints = RigidbodyConstraints.FreezeRotation;
+			Sphere.constraints = RigidbodyConstraints.FreezePositionY; 
+		}
+	}
+}
